5190f9446e403b6884e2d14c29f506596b419a1e
Frankwii <frank.william.hammond@gmail.com>
2025-04-22 20:08:38 +0200

    Implemented function semantic search

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,6 +1,7 @@
 import os
-from typing import Optional
+import numpy as np
 
+from typing import Literal, Optional
 from torch import Tensor
 
 from file_handling import IOHandler
@@ -8,9 +9,21 @@ from pathlib import Path
 
 from language_model import LLMHandler
 
+def centered_dot(a1: np.ndarray, a2: np.ndarray):
+    return np.dot(a1 - a1.mean(), a2 - a2.mean())
+
+
+def pearson(a1: np.ndarray, a2: np.ndarray):
+    m1 = a1.mean()
+    m2 = a2.mean()
+
+    return np.dot(a1-m1, a2-m2)/(a1.std(mean=m1) * a2.std(mean=m2))
+
+def cosine(a1: np.ndarray, a2: np.ndarray):
+    return np.dot(a1, a2)/(np.linalg.norm(a1) * np.linalg.norm(a2))
 
 class Pipeline:
-    def __init__(self, model_name: str):
+    def __init__(self, model_name: str, similarity_function: Literal["pearson", "centered_dot", "cosine", "dot"] = "cosine"):
         self.__IO_handler = IOHandler(self.__get_repo_path())
         self.__bacteria_functions = self.__IO_handler.get_bacteria_functions()
         self.__bacteria = list(self.__bacteria_functions.keys())
@@ -23,6 +36,16 @@ class Pipeline:
         self.__model_handler : Optional[LLMHandler] = None
 
         self.__function_embeddings = self.__get_function_embeddings()
+
+        match similarity_function:
+            case "dot":
+                self.__similarity_function = np.dot
+            case "centered_dot":
+                self.__similarity_function = centered_dot
+            case "pearson":
+                self.__similarity_function = pearson
+            case "cosine":
+                self.__similarity_function = cosine
 
     def __load_model_handler(self):
         if self.__model_handler is None:
@@ -61,3 +84,13 @@ class Pipeline:
 
     def get_bacteria(self) -> list[str]:
         return self.__bacteria
+
+    def function_semantic_search(self, query: str) -> list[str]:
+        self.__load_model_handler()
+        embedding_query = self.__model_handler.get_classification_embedding(query).flatten()
+
+        return sorted(
+            self.__function_embeddings.keys(),
+            key=lambda k: self.__similarity_function(np.array(self.__function_embeddings[k]), embedding_query),
+            reverse=True
+        )
